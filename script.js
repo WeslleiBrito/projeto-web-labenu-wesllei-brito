30753f0817b464c3ed0268aef45e8fe45e35ff2c
WeslleiBrito <wesllei.wbs@gmail.com>
2023-01-29 08:23:34 -0300

    Atualizando a pagina de matricula

--- script.js
+++ script.js
@@ -1,137 +1,159 @@
 const carrinhoCursos = []
 
 const cursos = [{
-        curso: 'HTML e CSS',
-        descricao: 'O curso é voltado para o desenvolvimento frontand',
-        duracao: '1 mês',
-        valor: 500.00,
-    },
+    curso: 'HTML e CSS',
+    descricao: 'O curso é voltado para o desenvolvimento frontand',
+    duracao: '1 mês',
+    valor: 500.00,
+},
 
-    {
-        curso: 'JavaScript',
-        descricao: 'Foco no backand ',
-        duracao: '2 mês',
-        valor: 900.00
-    },
+{
+    curso: 'JavaScript',
+    descricao: 'Foco no backand ',
+    duracao: '2 mês',
+    valor: 900.00
+},
 
-    {
-        curso: 'APIs REST',
-        descricao: 'Ferramenta para ligação entre sistemas',
-        duracao: '6 meses',
-        valor: 2000.00
-    }
+{
+    curso: 'APIs REST',
+    descricao: 'Ferramenta para ligação entre sistemas',
+    duracao: '6 meses',
+    valor: 2000.00
+}
 ]
+
 
 const turmas = [{
-        turma: 'Hipátia',
-        curso: 'JavaScript',
-        inicio: '30/11/2022',
-        termino: '30/01/2023',
-        numeroDeAlunos: 150,
-        periodo: 'Noturno',
-        concluido: false
-    },
+    turma: 'Hipátia',
+    curso: 'JavaScript',
+    inicio: '30/11/2022',
+    termino: '30/01/2023',
+    numeroDeAlunos: 150,
+    periodo: 'Noturno',
+    concluido: false
+},
 
-    {
-        turma: 'Sibyla',
-        curso: 'JavaScript',
-        inicio: '30/10/2022',
-        termino: '30/12/2022',
-        numeroDeAlunos: 200,
-        periodo: 'Integral',
-        concluido: false
-    },
+{
+    turma: 'Sibyla',
+    curso: 'JavaScript',
+    inicio: '30/10/2022',
+    termino: '30/12/2022',
+    numeroDeAlunos: 200,
+    periodo: 'Integral',
+    concluido: false
+},
 
-    {
-        turma: 'Curie',
-        curso: 'HTML e CSS',
-        inicio: '15/09/2022',
-        termino: '15/10/2022',
-        numeroDeAlunos: 180,
-        periodo: 'Noturno',
-        concluido: true
-    },
+{
+    turma: 'Curie',
+    curso: 'HTML e CSS',
+    inicio: '15/09/2022',
+    termino: '15/10/2022',
+    numeroDeAlunos: 180,
+    periodo: 'Noturno',
+    concluido: true
+},
 
-    {
-        turma: 'Zhenyi',
-        curso: 'HTML e CSS',
-        inicio: '01/11/2022',
-        termino: '01/01/2023',
-        numeroDeAlunos: 80,
-        periodo: 'Integral',
-        concluido: false
-    },
+{
+    turma: 'Zhenyi',
+    curso: 'HTML e CSS',
+    inicio: '01/11/2022',
+    termino: '01/01/2023',
+    numeroDeAlunos: 80,
+    periodo: 'Integral',
+    concluido: false
+},
 
-    {
-        turma: 'Clarke',
-        curso: 'HTML e CSS',
-        inicio: '04/07/2022',
-        termino: '04/09/2022',
-        numeroDeAlunos: 200,
-        periodo: 'Noturno',
-        concluido: true
-    },
+{
+    turma: 'Clarke',
+    curso: 'HTML e CSS',
+    inicio: '04/07/2022',
+    termino: '04/09/2022',
+    numeroDeAlunos: 200,
+    periodo: 'Noturno',
+    concluido: true
+},
 
-    {
-        turma: 'Blackwell',
-        curso: 'APIsREST',
-        inicio: '20/03/2022',
-        termino: '20/06/2022',
-        numeroDeAlunos: 100,
-        periodo: 'Noturno',
-        concluido: true
-    },
+{
+    turma: 'Blackwell',
+    curso: 'APIsREST',
+    inicio: '20/03/2022',
+    termino: '20/06/2022',
+    numeroDeAlunos: 100,
+    periodo: 'Noturno',
+    concluido: true
+},
 
-    {
-        turma: 'Elion',
-        curso: 'APIsREST',
-        inicio: '12/01/2022',
-        termino: '12/06/2022',
-        numeroDeAlunos: 200,
-        periodo: 'Noturno',
-        concluido: true
-    },
+{
+    turma: 'Elion',
+    curso: 'APIsREST',
+    inicio: '12/01/2022',
+    termino: '12/06/2022',
+    numeroDeAlunos: 200,
+    periodo: 'Noturno',
+    concluido: true
+},
 
-    {
-        turma: 'Burnell',
-        curso: 'APIsREST',
-        inicio: '18/10/2022',
-        termino: '18/04/2023',
-        numeroDeAlunos: 90,
-        periodo: 'Integral',
-        concluido: false
-    },
+{
+    turma: 'Burnell',
+    curso: 'APIsREST',
+    inicio: '18/10/2022',
+    termino: '18/04/2023',
+    numeroDeAlunos: 90,
+    periodo: 'Integral',
+    concluido: false
+},
 ]
 
+
+const verificarTurmasAbertas = (nomeCurso = "") =>{
+    if(nomeCurso){
+        return turmas.filter(objeto => (objeto.curso.toLowerCase().includes(nomeCurso.toLowerCase()) && !objeto.concluido))
+    }
+
+    return turmas.filter(objeto => (!objeto.concluido))
+    
+}
+
+const turmasAbertas = verificarTurmasAbertas()
+
+
+function renderizaOpcoesNoHtml(){
+    const caixaDeSelecao = document.getElementById('cursos')
+    console.log(caixaDeSelecao.value)
+}
+
+renderizaOpcoesNoHtml()
+
+
 const estutantes = [{
-        estudante: 'Chris Evans',
-        turma: 'Hipátia',
-        curso: 'Javascript',
-        valor: 900.00,
-        nParcelas: 9,
-        desconto: false,
-        parcelas: 100.00
-    },
+    estudante: 'Chris Evans',
+    turma: 'Hipátia',
+    curso: 'Javascript',
+    valor: 900.00,
+    nParcelas: 9,
+    desconto: false,
+    parcelas: 100.00
+},
 
-    {
-        estudante: 'Halle Berry',
-        turma: 'Burnel',
-        curso: 'APIsRest',
-        valor: 2000.00,
-        nParcelas: '4',
-        desconto: false,
-        parcelas: 4
-    },
+{
+    estudante: 'Halle Berry',
+    turma: 'Burnel',
+    curso: 'APIsRest',
+    valor: 2000.00,
+    nParcelas: '4',
+    desconto: false,
+    parcelas: 4
+},
 
-    {
-        estudante: 'Lashana lynch',
-        turma: 'Zhenyi',
-        curso: 'HTML e CSS',
-        valor: 500.00,
-        nParcelas: 1,
-        desconto: true,
-        parcelas: 500.00
-    },
+{
+    estudante: 'Lashana lynch',
+    turma: 'Zhenyi',
+    curso: 'HTML e CSS',
+    valor: 500.00,
+    nParcelas: 1,
+    desconto: true,
+    parcelas: 500.00
+},
 ]
 
 function arredonadaParaCima(valor) {
@@ -185,7 +207,7 @@ function parelarCurso(parcela, curso, arrayDeValores) {
 
 function buscarCurso(nomeCurso) {
 
-    const dadosCurso = cursos.find(objeto => objeto.curso === nomeCurso)
+    const dadosCurso = cursos.filter(objeto => objeto.curso.includes(nomeCurso))
 
     return dadosCurso
 }
